d3c6ee929e518fb3e0064c0e65edf0f85456a1b6
Jefferson Doljak <jeffersondoljak@MacBook-Pro.local>
2022-11-19 17:44:00 -0300

    início do processo

---
+++ jest.config.tsx
@@ -0,0 +1,26 @@
+export const presets = {
+  roots: ["<rootDir>/src"],
+  preset: "ts-jest",
+  coverageDirectory: "coverage",
+  collectCoverage: false,
+  collectCoverageFrom: [
+    "src/**/*.{js,jsx,ts,tsx}",
+    "!<rootDir>/src/**/*mock*.{js,jsx,ts,tsx}",
+    "!<rootDir>/src/styles/**/*.{js,jsx,ts,tsx}",
+    "!<rootDir>/src/config/**/*.{js,jsx,ts,tsx}",
+    "!<rootDir>/src/pages/**/*.{js,jsx,ts,tsx}",
+    "!<rootDir>/**/stories.{js,jsx,ts,tsx}",
+    "!<rootDir>/node_modules/",
+  ],
+  testEnvironment: "jsdom",
+  testMatch: [
+    "**/__tests__/**/*.[jt]s?(x)",
+    "**/?(*.)+(spec|test|tests).[tj]s?(x)",
+  ],
+  testPathIgnorePatterns: ["/node_modules/", "/.next/", "/.out/", "/public/"],
+  setupFilesAfterEnv: ["<rootDir>/.jest/setup-tests.ts"],
+  transform: {
+    "\\.tsx?$": "ts-jest",
+    "\\.jsx?$": "babel-jest",
+  },
+};
